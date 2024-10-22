24d03705c4a6910e120a1568e9e8827aa913648f
Charles <charlez@cs.washington.edu>
2024-10-22 15:13:14 -0700

    Getting the bare bones out of the way...

---
+++ ProofTrack/app/(tabs)/Profile.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+
+function Profile(props: any) {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+export default Profile;
