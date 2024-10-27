62d159d2cf754323779e501cdfcec9ea263cd0b7
kse8 <kse8@cs.washington.edu>
2024-10-26 17:56:26 -0700

    Added tutorial text to main page

--- ProofTrack/app/(tabs)/index.tsx
+++ ProofTrack/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { Text, TextStyle, View, TouchableOpacity, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,14 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>This will function as the home page.</Text>
+      <Text style={styles.header1}>Welcome to ProofTrack!</Text>
+      <Text style={styles.body}>
+        First set up your password under the Profile tab!{'\n'}
+        After you've set up a password it's time to start on your first project! {'\n'}
+        Click on the New Project tab to specify the parameters for a new project. {'\n'}
+        Once your project is set up, ProofTrack will schedule persistent reminders for you. {'\n'}
+        To stop being reminded, simply submit proof of your progress.
+      </Text>
 
       <TouchableOpacity
         //onPress={navigation.navigate('Profile', {name: 'Jane'})}
@@ -17,7 +24,6 @@ export default function Index() {
         <Text>I'm a button</Text>
       </TouchableOpacity>
     </View>
-    
   );
 
 }
@@ -33,4 +39,16 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     backgroundColor: '#4a91e2',
   },
+  header1: {
+    fontFamily: 'Arial',
+    fontSize: 28,
+    justifyContent: 'center',
+    alignContent: 'flex-start',
+  },
+  body: {
+    fontFamily: 'Arial',
+    fontSize: 18,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+  }
 });
