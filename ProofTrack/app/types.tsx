005cf9f6f1b0884f36b3c61848b62b8e714b196e
Charles <charlez@cs.washington.edu>
2024-10-31 16:46:44 -0700

    I added a types file where we can specify types.

---
+++ ProofTrack/app/types.tsx
@@ -0,0 +1,7 @@
+/* Basic information on a user. */
+export type User = {
+  /* A user's username, such as Kyle, Anmol, or Charles. */
+  username: string,
+  /* A user's UNIQUE ID number. */
+  userID: number
+}
