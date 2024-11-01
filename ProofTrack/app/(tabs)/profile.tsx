29d4512d480fccbbb079808c58eb5e25daf8f0e3
Charles <charlez@cs.washington.edu>
2024-11-01 14:24:44 -0700

    Populating the Profile tab!

--- ProofTrack/app/(tabs)/profile.tsx
+++ ProofTrack/app/(tabs)/profile.tsx
@@ -1,5 +1,6 @@
 import React, {useContext } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { Link } from 'expo-router';
 import { UserContext } from './_layout';
 import { User } from '../types';
@@ -8,28 +9,44 @@ export default function Profile() {
   const user: User = useContext(UserContext);
   return (
     <View style={styles.container}>
-      <Text style={styles.leftLabel}>Name: {user.username}</Text>
-      <Link href="./project_list" style={styles.rightLink}>{"[Edit]"}</Link>
-    </View>   
+      <View style={styles.profile_icon}>
+        <Ionicons name="person" size={64} color="#25292e" />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.leftText}>Username</Text>
+        <Link href="../edit_username" style={styles.rightLink}>{"[Edit]\n"}</Link>
+      </View> 
+      <View style={styles.row}>
+        <Text style={styles.leftText}>Password</Text>
+        <Link href="../edit_username" style={styles.rightLink}>{"[Edit]"}</Link>
+      </View>   
+    </View> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    //justifyContent: "center",
-    //alignItems: "center",
+    // Placholder so we at least get a default!
   },
-  leftLabel: {
+  profile_icon: {
+    alignItems: "center",
+    top: 37.5,
+  },
+  row: {
+    flexDirection: "row",
+    top: 80,
+  },
+  leftText: {
+    //flex: 5,
     fontSize: 20,
     fontWeight: "bold",
-    left: 60,
-    top: 120,
+    left: 20,
+    textAlign: "left",
   },
   rightLink: {
     fontSize: 20,
     fontWeight: "bold",
-    left: 260,
-    top: 120,
-  }
+    color: '#4A90E2',
+    left: 180,
+  },
 });
