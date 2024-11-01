590d17aff01d856d33a662f480b3bb0cc773094d
Charles <charlez@cs.washington.edu>
2024-11-01 13:17:51 -0700

    Working on styling "profile.tsx"

--- ProofTrack/app/(tabs)/profile.tsx
+++ ProofTrack/app/(tabs)/profile.tsx
@@ -1,21 +1,35 @@
 import React, {useContext } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Link } from 'expo-router';
 import { UserContext } from './_layout';
 import { User } from '../types';
 
 export default function Profile() {
   const user: User = useContext(UserContext);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>
-        Edit your profile, {user.username}?
-      </Text>
-    </View>
+    <View style={styles.container}>
+      <Text style={styles.leftLabel}>Name: {user.username}</Text>
+      <Link href="./project_list" style={styles.rightLink}>{"[Edit]"}</Link>
+    </View>   
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //justifyContent: "center",
+    //alignItems: "center",
+  },
+  leftLabel: {
+    fontSize: 20,
+    fontWeight: "bold",
+    left: 60,
+    top: 120,
+  },
+  rightLink: {
+    fontSize: 20,
+    fontWeight: "bold",
+    left: 260,
+    top: 120,
+  }
+});
