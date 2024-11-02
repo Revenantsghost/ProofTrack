908b429a3fd5bb84c5d84b34ecba929c6ccad99a
Charles <charlez@cs.washington.edu>
2024-11-01 22:17:25 -0700

    Continuing to style the profile page.

--- ProofTrack/app/(tabs)/profile.tsx
+++ ProofTrack/app/(tabs)/profile.tsx
@@ -8,45 +8,57 @@ import { User } from '../types';
 export default function Profile() {
   const user: User = useContext(UserContext);
   return (
-    <View style={styles.container}>
-      <View style={styles.profile_icon}>
+    <View>
+      <View style={{ alignItems: "center", top: 37.5 }}>
         <Ionicons name="person" size={64} color="#25292e" />
       </View>
-      <View style={styles.row}>
+      <Text style={styles.profileName}>
+        {appendApostrophe(user.username)} Profile
+      </Text>
+      <View style={{ flexDirection: "row", top: 80 }}>
         <Text style={styles.leftText}>Username</Text>
-        <Link href="../edit_username" style={styles.rightLink}>{"[Edit]\n"}</Link>
-      </View> 
-      <View style={styles.row}>
+        <Link href="../edit_username" style={styles.rightLink}>{"[Edit]"}</Link>
+      </View>
+      <View style={{ flexDirection: "row", top: 120 }}>
         <Text style={styles.leftText}>Password</Text>
         <Link href="../edit_username" style={styles.rightLink}>{"[Edit]"}</Link>
-      </View>   
+      </View>
     </View> 
   );
 }
 
+/* Turns a name into its possessive form.
+ * Ex: Kyle -> Kyle's
+ * Ex: Charles -> Charles' */
+function appendApostrophe(name: string): string {
+  const lastChar: string = name[name.length - 1];
+  if (lastChar == "s" || lastChar == "S") {
+    return name + "'";
+  } else {
+    return name + "'s";
+  }
+}
+
 const styles = StyleSheet.create({
-  container: {
-    // Placholder so we at least get a default!
-  },
-  profile_icon: {
-    alignItems: "center",
-    top: 37.5,
-  },
-  row: {
-    flexDirection: "row",
-    top: 80,
-  },
   leftText: {
-    //flex: 5,
+    position: "absolute",
     fontSize: 20,
     fontWeight: "bold",
-    left: 20,
+    left: 30,
     textAlign: "left",
   },
+  profileName: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    top: 50,
+  },
   rightLink: {
+    position: "absolute",
     fontSize: 20,
     fontWeight: "bold",
     color: '#4A90E2',
-    left: 180,
+    textAlign: "right",
+    right: 30,
   },
 });
