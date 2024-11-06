8a4908d54c5fce8d11fb98f82ba519fe07d654e1
anmolkb <anmolbhuller21@gmail.com>
2024-11-05 21:46:47 -0800

    Merged new_project_page_beta1 and resolved conflicts

--- ProofTrack/app/(tabs)/profile.tsx
+++ ProofTrack/app/(tabs)/profile.tsx
@@ -1,21 +1,86 @@
 import React, {useContext } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Link } from 'expo-router';
 import { UserContext } from './_layout';
 import { User } from '../types';
 
+/* UI for user's profile page.
+ * Displays their username and allows them to change username or password.
+ * Also displays some stats, such as number of projects in progress. */
 export default function Profile() {
+  // Grab the current user's information.
   const user: User = useContext(UserContext);
+  // Convert their username into its possessive form.
+  const theirs: string = appendApostrophe(user.username);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>
-        Edit your profile, {user.username}?
-      </Text>
-    </View>
+    <View>
+      { /* Positioning for profile icon. */}
+      <View style={{ alignItems: "center", top: 30 }}>
+        <Ionicons name="person" size={64} color="#25292e" />
+      </View>
+      { /* User profile header text. */ }
+      <View style={{ top: 50 }}>
+        <Text style={styles.titleText}>{theirs} Profile</Text>
+      </View>
+      { /* Row providing link to change username. */ }
+      <View style={{ top: 70 }}>
+        <Text style={styles.leftText}>Username</Text>
+        <Link href="../profile" style={styles.rightLink}>{"[Edit]"}</Link>
+      </View>
+      { /* Row providing link to change password. */ }
+      <View style={{ top: 120 }}>
+        <Text style={styles.leftText}>Password</Text>
+        <Link href="./profile" style={styles.rightLink}>{"[Edit]"}</Link>
+      </View>
+      { /* User statistics header text. */ }
+      <View style={{ top: 210 }}>
+        <Text style={styles.titleText}>{theirs} Statistics</Text>
+      </View>
+      { /* Row displaying user's number of projects. */ }
+      <View style={{ top: 230 }}>
+        <Text style={styles.leftText}>Projects</Text>
+        <Text style={styles.rightText}>{user.numProjects}</Text>
+      </View>
+    </View> 
   );
 }
+
+/* Returns a name in its possessive form.
+ * Ex: Kyle -> Kyle's
+ * Ex: Charles -> Charles' */
+function appendApostrophe(name: string): string {
+  const lastChar: string = name[name.length - 1];
+  if (lastChar.toLowerCase() == "s") {
+    return name + "'";
+  } else {
+    return name + "'s";
+  }
+}
+
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  leftText: {
+    position: "absolute",
+    left: 30,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  rightText: {
+    position: "absolute",
+    right: 30,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  rightLink: {
+    position: "absolute",
+    right: 30,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#4A90E2",
+  },
+});
