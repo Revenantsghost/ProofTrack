156edf4662912901d407d8a9134c02328db2f8e0
Charles <charlez@cs.washington.edu>
2024-11-05 20:14:50 -0800

    Added a field to the User type and modified files using it accordingly.

--- ProofTrack/app/(tabs)/profile.tsx
+++ ProofTrack/app/(tabs)/profile.tsx
@@ -40,7 +40,7 @@ export default function Profile() {
       { /* Row displaying user's number of projects. */ }
       <View style={{ top: 230 }}>
         <Text style={styles.leftText}>Projects</Text>
-        <Text style={styles.rightText}>{/* user.numProjects? */"X"}</Text>
+        <Text style={styles.rightText}>{user.numProjects}</Text>
       </View>
     </View> 
   );
