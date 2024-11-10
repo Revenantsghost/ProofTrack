3a9d546e63de108266e645de60b7d043e5d98b79
anmolkb <anmolbhuller21@gmail.com>
2024-11-10 11:39:07 -0800

    worked on submit proof camera page, live camera functionality works!

--- ProofTrack/app/submit_proof.tsx
+++ ProofTrack/app/submit_proof.tsx
@@ -1,19 +1,78 @@
-import React, { useContext } from "react";
-import { Text, View } from "react-native";
+import React, { useContext, useState, useEffect } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { UserContext } from './(tabs)/_layout';
 import { User } from './types';
+import { Camera, CameraType, CameraView, useCameraPermissions } from "expo-camera";
+import * as MediaLibrary from 'expo-media-library';
+import { GestureHandlerRootView, TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Index() {
   const user: User = useContext(UserContext);
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+  const [image, setImage] = useState(null);
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <GestureHandlerRootView>
+        <View style={styles.container}>
+          <Text style={styles.message}>We need your permission to show the camera</Text>
+          <Button onPress={requestPermission} title="grant permission" />
+        </View>
+      </GestureHandlerRootView>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>You'll want to submit proof here, {user.username}.</Text>
-    </View>
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <CameraView style={styles.camera} facing={facing}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+              <Text style={styles.text}>Flip Camera</Text>
+            </TouchableOpacity>
+          </View>
+        </CameraView>
+      </View>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
