989b7a5e542390a9f8d54fc7e89d9209dc288027
Charles <charlez@cs.washington.edu>
2024-12-04 12:33:28 -0800

    Lint-picking constants.ts

--- ProofTrack/app/constants.ts
+++ ProofTrack/app/constants.ts
@@ -1,10 +1,10 @@
 /* The IP of our server. */
-export const SERVER_IP: string = '13.64.145.249'
+export const SERVER_IP: string = '13.64.145.249';
 
 /* The port our server runs on. */
-export const SERVER_PORT: string = '3000'
+export const SERVER_PORT: string = '3000';
 
-/* Returns the link of the server as a string.
+/* Returns the link of our server as a string.
  * Format: 'http://XX.XX.XXX.XXX:PORT' */
 export function getServer(): string {
     return `http://${SERVER_IP}:${SERVER_PORT}`;
