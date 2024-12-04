0b2c0d5e82cb6373b475370c7f1ffaf1a3c8fd5e
Charles <charlez@cs.washington.edu>
2024-12-03 17:17:25 -0800

    Restyled "getServer()" in constants.ts

--- ProofTrack/app/constants.ts
+++ ProofTrack/app/constants.ts
@@ -7,5 +7,5 @@ export const SERVER_PORT: string = '3000'
 /* Returns the link of the server as a string.
  * Format: 'http://XX.XX.XXX.XXX:PORT' */
 export function getServer(): string {
-    return "http://" + SERVER_IP + ":" + SERVER_PORT;
+    return `http://${SERVER_IP}:${SERVER_PORT}`;
 }
