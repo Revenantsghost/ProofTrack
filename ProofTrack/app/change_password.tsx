f86c50ecbea73157541442888968eb59685b37c2
Charles <charlez@cs.washington.edu>
2024-12-03 20:05:54 -0800

    Working on connecting "Change Password" to the backend.

--- ProofTrack/app/change_password.tsx
+++ ProofTrack/app/change_password.tsx
@@ -12,34 +12,44 @@ export default function ChangePassword() {
   /* Parse username from the expo router. */
   var { username } = useLocalSearchParams();
   const usernameStr: string = (typeof(username) === 'string') ? (username) : ('');
-  /* The new user's password entered when creating an account. */
-  const [password, setPassword] = useState('');
-  /* Password and PasswordConfirm MUST be equal. */
-  const [passwordConfirm, setPasswordConfirm] = useState('');
+  /* The user's current password. */
+  const [currPassword, setCurrPassword] = useState('');
+  /* The user's target new password. */
+  const [newPassword, setNewPassword] = useState('');
+  /* NewPassword and NewPasswordConfirm MUST be equal. */
+  const [newPasswordConfirm, setNewPasswordConfirm] = useState('');
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Change Password</Text>
+      <Text style={{fontSize: 18, textAlign: "left" }}>Current Password</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Current Password"
+        value={currPassword}
+        onChangeText={setCurrPassword}
+        secureTextEntry
+      />
       <Text style={{fontSize: 18, textAlign: "left" }}>New Password</Text>
       <TextInput
         style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
+        placeholder="New Password"
+        value={newPassword}
+        onChangeText={setNewPassword}
         secureTextEntry
       />
       <Text style={{fontSize: 18, textAlign: "left" }}>Confirm New Password</Text>
       <TextInput
         style={styles.input}
-        placeholder="Confirm Password"
-        value={passwordConfirm}
-        onChangeText={setPasswordConfirm}
+        placeholder="Confirm New Password"
+        value={newPasswordConfirm}
+        onChangeText={setNewPasswordConfirm}
         secureTextEntry
       />
 
       <Pressable
         style={styles.button}
-        onPress={() => {handleChange(usernameStr, password, passwordConfirm)}}
+        onPress={() => {handleChange(usernameStr, currPassword, newPassword, newPasswordConfirm)}}
       >
         <Text style={styles.buttonText}>Change Password</Text>
       </Pressable>
@@ -48,23 +58,28 @@ export default function ChangePassword() {
 };
 
 /* Attempts to send a new user to the database.
- * Returns false and throws error if username doesn't exist (which is an internal issue).
+ * Returns false if username doesn't exist (which is an internal issue).
  * Also returns false and throws error if server error encountered.
  * Returns true if no issues encountered. */
-async function handleChange(username: string, password: string, passwordConfirm: string) {
+async function handleChange(username: string, currPassword: string,
+                            newPassword: string, newPasswordConfirm: string) {
   if (!username) {
     /* This case should NEVER happen. */
     Alert.alert('Internal error encountered.', 'Please try again later.');
     return;
-  } else if (!password) {
+  } else if (!currPassword) {
     /* Make sure user entered their password. */
     Alert.alert('Error', 'Please enter new password.');
     return;
-  } else if (!passwordConfirm) {
+  } else if (!newPassword) {
+    /* Make sure user entered their password. */
+    Alert.alert('Error', 'Please enter new password.');
+    return;
+  } else if (!newPasswordConfirm) {
     /* Make sure user confirmed their password. */
     Alert.alert('Error', 'Please confirm new password.');
     return;
-  } else if (password !== passwordConfirm) {
+  } else if (newPassword !== newPasswordConfirm) {
     /* Make sure "Password" and "Confirm Password" match. */
     Alert.alert('Error', 'New passwords do not match.');
     return;
@@ -72,7 +87,7 @@ async function handleChange(username: string, password: string, passwordConfirm:
 
   /* Change password for user in the database.
    * Handles case of username not being found (a major internal error). */
-  const changeAttempt: boolean = await newPassword(username, password);
+  const changeAttempt: boolean = await editPassword(username, currPassword, newPassword);
   if (changeAttempt) {
     /* Success! */
     Alert.alert('Success!', 'Password successfully changed.');
@@ -81,24 +96,37 @@ async function handleChange(username: string, password: string, passwordConfirm:
   }
 }
 
-async function newPassword(user_name: string, password: string): Promise<boolean> {
+async function editPassword(user_name: string, old_password: string,
+                            new_password: string): Promise<boolean> {
   try {
     // Fix to correct route and method type!!
-    const route: string = 'nonsense';
+    const route: string = '/changePassword';
     const response: Response = await fetch(`${SERVER}/${route}`, {
-      method: 'DO NOT KNOW',
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ user_name: user_name, password: password })
+      body: JSON.stringify({
+        user_name: user_name,
+        new_password: new_password,
+        old_password: old_password,
+      })
     });
     // Fix to correct response status!!
-    const no_username_status: number = 0;
-    if (response.status === no_username_status) {
+    const no_username_status: number = 404;
+    const wrong_password_status: number = 401;
+    if (response.status == wrong_password_status) {
+      /* The user didn't input their correct password right.
+       * Not an internal error, so just return false. */
+      Alert.alert('Error', 'Your current password is incorrect.');
+      return false;
+    } else if (response.status === no_username_status) {
+      console.log(user_name);
       /* A status of XXX means this username doesn't exist.
-       * As user has already created an account, this is an error on our end. */
+       * As user has already created an account, this is an internal error.
+       * Not a server error, so don't throw an error. */
       Alert.alert('Error', 'We\'re having trouble accessing your information.');
-      throw new Error('Internal error');
+      return false;
     } else if (!response.ok) {
       /* A server error is most definitely an internal error.
        * Throw the error and let the catch block handle it. */
