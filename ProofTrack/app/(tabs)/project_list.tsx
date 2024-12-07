b372ba64d7baab44930e02f2de02dfb192654a62
Charles <charlez@cs.washington.edu>
2024-12-07 09:33:29 -0800

    Cleaned up list_of_projects abstractions and added more documentation.

--- ProofTrack/app/(tabs)/project_list.tsx
+++ ProofTrack/app/(tabs)/project_list.tsx
@@ -4,13 +4,23 @@ import { UserContext } from './_layout';
 import { router } from 'expo-router';
 import { User } from '../types';
 
-
-
+/** 
+ * Renders an individual item row in the list.
+ * 
+ * @returns {JSX.Element} A list of pressable item row components.
+ * Pressing a component will allow the user to view its corresponding project.
+ */
 export default function ProjectList() {
   /* Use hook to get username. */
   const user: User = useContext(UserContext);
 
-  const goToEditProject = (projID: number) => {
+  /** 
+   * Renders an individual item row in the list.
+   * 
+   * @param projID A project's unique ID "number".
+   * Takes the user to the "View/Edit Project" page corresponding to the projID.
+   */
+  const goToEditProject = (projID: string) => {
     console.log(`Going to edit project number: ${projID}`);
     router.navigate(`../edit_project?userID=${user.username}&projID=${projID}`);
   }
@@ -18,7 +28,7 @@ export default function ProjectList() {
   return (
     <ListOfProjects 
       username={user.username}
-      handleProjectPress={(projID: number) => {goToEditProject(projID)}}
+      handleProjectPress={(projID: string) => {goToEditProject(projID)}}
     />
   );
 }
